b1b3ea7572e867becabb69311b2cb867585d35aa
Michael Cassano <mcassano@gmail.com>
2015-08-23 10:33:29 -0600

    step 2 tutorial, show 00:00

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
   
 static void main_window_load(Window *window) {
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
 }
 
@@ -21,6 +31,7 @@ static void init() {
 }
 
 static void deinit() {
+  text_layer_destroy(s_time_layer);
   window_destroy(s_main_window); 
 }
 
